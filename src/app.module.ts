81857ba382377de594b584d27270b66380c492ac
jefer15 <70409281+jefer15@users.noreply.github.com>
2025-05-26 22:39:47 -0500

    feat: application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
